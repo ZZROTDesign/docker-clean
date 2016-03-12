b266ba2b761c27f9d5eb05d592a4384839735239
killianbrackey <killianbrackey@gmail.com>
2016-03-12 12:55:38 -0600

    Added docker-machine restart

--- docker-clean.sh
+++ docker-clean.sh
@@ -107,13 +107,25 @@ function dockerClean {
 	fi
 }
 
+#Restarts and reRuns docker-machine env active machine
+function restartMachine {
+	active="$(docker-machine active)"
+	docker-machine restart $active
+	eval $(docker-machine env $active)
+	echo Running docker-machine env $active...
+	echo "New IP Address for" $active":" $(docker-machine ip)
+}
+
 #Driver with options
 declare HELP_MENU=false
+declare RESTART=true
 Check
 while [ "$1" != "" ]; do
 	case $1 in 
-		-i | -a | --images) deleteImages ;;
+		-i | -a | --images) deleteImages ;; 
 		-f | --force) stopContainers ;;
+		-r | --reset) deleteImages 
+						RESTART=true ;;
 		-h | --help) HELP_MENU=true
 		echo Options: 
 		echo "		"-a or -i or --images "to stop and delete all Containers and Images"
@@ -127,3 +139,8 @@ done
 if [ $HELP_MENU == false ]; then
 	dockerClean
 fi
+
+if [ $RESTART == true ]; then
+	restartMachine
+fi
+	
