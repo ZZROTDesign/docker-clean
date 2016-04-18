b59bc704f8f1948bee517af411fbfa988f5d486f
Killian Brackey <killianbrackey@gmail.com>
2016-04-18 16:45:18 -0500

    regex checking

--- batsTests.sh
+++ batsTests.sh
@@ -21,7 +21,7 @@
   #run docker stop $(docker ps -a -q)
   run ./docker-clean -c
   [[ ${lines[0]} =~ "Cleaning containers..." ]]
-  [[ ${lines[1]} =~ "Containers cleaned: 1" ]]
+  [[ ${lines[1]} =~ "Containers cleaned:*" ]]
   run ./docker-clean -i
   [[ ${lines[1]} =~ "Cleaning Images..."  ]]
   [[ ${lines[2]} =~ "Images cleaned: 4" ]]
