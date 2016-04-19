604a7ce037a2766e31b8103083427623d39297fb
Killian Brackey <killianbrackey@gmail.com>
2016-04-18 21:58:29 -0500

    Added testOS function

--- playground.sh
+++ playground.sh
@@ -16,10 +16,13 @@
 function testOS {
   if [ "$(uname)" == "Darwin" ]; then
       # Do something under Mac OS X platform
+      echo MacOsX
   elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       # Do something under GNU/Linux platform
+      echo Linux
   elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
       # Do something under Windows NT platform
+      echo Windows
   fi
 }
 
